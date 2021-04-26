74e8f44b5a652e69e2e2b8b8b78b76de26434a7e
santiago <santiago.ottolini97@hotmail.com>
2021-04-26 12:26:42 -0300

    TS: Start

---
+++ index.js
@@ -0,0 +1,5 @@
+function add(numerOne, numerTwo) {
+    return numerOne + numerTwo;
+}
+console.log(add(2, 3));
+//tsc index.ts for excute code typescript
---
+++ index.ts
@@ -0,0 +1,7 @@
+function add(numerOne: number, numerTwo: number) {
+  return numerOne + numerTwo;
+}
+
+console.log(add(2, 3));
+
+//tsc index.ts for excute code typescript
