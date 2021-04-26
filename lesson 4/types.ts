802a755e91ffb656c7f10a4e616fc1df2d7dfd63
santiago <santiago.ottolini97@hotmail.com>
2021-04-26 16:18:46 -0300

    TS: Objects

---
+++ lesson 4/types.ts
@@ -0,0 +1,14 @@
+let person: {
+  name: string;
+  edad: number;
+  adress: {
+      number: number
+  }
+} = {
+  name: "Ramiro",
+  edad: 19,
+  adress: {
+      number: 5
+  }
+};
+console.log(person)
