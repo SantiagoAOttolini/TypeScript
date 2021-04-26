21bd506a9cd84aec41ebd1577fc6befd3cb9eda2
santiago <santiago.ottolini97@hotmail.com>
2021-04-26 13:03:00 -0300

    Type inference vs type anotation

---
+++ lesson 2/index.ts
@@ -0,0 +1,10 @@
+function NumberLessonTwo(lesNroOne, lesNroTwo) {
+  return lesNroOne + lesNroTwo;
+}
+
+let lesNroOne: number;
+let lesNroTwo: number;
+
+lesNroOne = 5;
+lesNroTwo = 6;
+console.log(NumberLessonTwo(numberOne, lesNroTwo));
